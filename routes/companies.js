7daa403b07f4a1eda9bea20e0d32b7ca8c4e88ad
Petar Petrov <ppankovp@gmail.com>
2020-07-20 17:52:57 +0300

    log

--- routes/companies.js
+++ routes/companies.js
@@ -2,12 +2,23 @@ const { Companies } = require('../models');
 
 module.exports = app => {
     app.get("/get-companies", async (req, res) => {
-    const companies = await Companies.findAll();
-    if (!companies.length) {
-      res.status(204).send();
-    } else {
-      res.send(companies);
-    }
-  });
+        const companies = await Companies.findAll();
+        if (!companies.length) {
+            res.status(204).send();
+        } else {
+            res.send(companies);
+        }
+    });
+
+    app.get("/get-company", async (req, res) => {
+       /*  const { id } = req.query; */
+       console.log(req)
+        const companies = await Companies.findAll({ where: { 'a' } });
+        if (!companies.length) {
+            res.status(204).send();
+        } else {
+            res.send(companies);
+        }
+    });
 };
 
