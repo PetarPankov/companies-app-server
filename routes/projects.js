9d49a5b30be5ce405b8d2964a808338369712551
Petar Petrov <ppankovp@gmail.com>
2020-07-20 18:34:49 +0300

    test:

--- routes/projects.js
+++ routes/projects.js
@@ -6,18 +6,23 @@ module.exports = app => {
         if (!projects.length) {
             res.status(204).send();
         } else {
-            res.send(projects);
+            res.send({ status: 'success', data: projects });
         }
     });
 
     app.get("/get-projects-by-company-id", async (req, res) => {
         const { id } = req.query;
-         const projects = await Projects.findAll({ where: { companyId: id } });
-         if (!projects.length) {
-             res.status(204).send();
-         } else {
-             res.send(projects);
-         }
-     });
+        const projects = await Projects.findAll({ where: { companyId: id } });
+        if (!projects.length) {
+            res.send({
+                status: 'error',
+                data: {
+                    errorText: 'No projects found'
+                }
+            });
+        } else {
+            res.send({ status: 'success', data: projects });
+        }
+    });
 };
 
