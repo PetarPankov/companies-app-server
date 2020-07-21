8362685ac2a415aecb4a1a35dd902144f8a36968
Petar Petrov <ppankovp@gmail.com>
2020-07-21 14:48:36 +0300

    feat: add get project by id

--- routes/projects.js
+++ routes/projects.js
@@ -10,6 +10,16 @@ module.exports = app => {
         }
     });
 
+    app.get("/get-project-by-id", async (req, res) => {
+        const { id } = req.query;
+        const project = await Projects.findAll({ where: { id, isDeleted: 0 } });
+        if (!project.length) {
+            res.end([]);
+        } else {
+            res.send(project[0]);
+        }
+    });
+
     app.get("/get-projects-by-company-id", async (req, res) => {
         const { id } = req.query;
         const projects = await Projects.findAll({ where: { companyId: id, isDeleted: 0 } });
